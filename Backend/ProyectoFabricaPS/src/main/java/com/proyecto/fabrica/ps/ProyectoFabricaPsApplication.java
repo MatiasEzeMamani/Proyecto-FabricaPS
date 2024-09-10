07766f97dc98008786bd4092645832cab53480d1
Matias Mamani <matias.ezequiel.1177@gmail.com>
2024-09-10 12:41:24 -0300

    First commit

---
+++ Backend/ProyectoFabricaPS/src/main/java/com/proyecto/fabrica/ps/ProyectoFabricaPsApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.fabrica.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFabricaPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFabricaPsApplication.class, args);
+	}
+
+}
---
+++ Backend/ProyectoFabricaPS/src/test/java/com/proyecto/fabrica/ps/ProyectoFabricaPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.fabrica.ps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFabricaPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
